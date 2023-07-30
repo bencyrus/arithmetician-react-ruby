f7e2b953908602c25ff01235afb22b48da522d4e
Ben Cyrus <62337982+bencyrus@users.noreply.github.com>
2023-07-30 02:13:26 +0000

    added basic game functionality

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,20 +1,109 @@
-import { useContext } from 'react'
+import { useContext, useState, useRef, useEffect, useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
 import GameContext from '../context/GameContext'
 
 const GamePage = () => {
-	const { questions } = useContext(GameContext)
+	const {
+		additionRange,
+		multiplicationRange,
+		duration,
+		questions,
+		setQuestions,
+		score,
+		setScore,
+	} = useContext(GameContext)
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+	const [timer, setTimer] = useState(duration)
+	const [answeredQuestions, setAnsweredQuestions] = useState([])
+	const inputRef = useRef()
+	const navigate = useNavigate()
+
+	const setGame = useCallback(() => {
+		console.log({
+			additionRange,
+			multiplicationRange,
+			duration,
+			score,
+			endTimestamp: new Date(),
+			answeredQuestions,
+		})
+		setQuestions([])
+		setCurrentQuestionIndex(0)
+		setAnsweredQuestions([])
+		setTimer(duration)
+		navigate('/score')
+	}, [
+		additionRange,
+		multiplicationRange,
+		duration,
+		score,
+		answeredQuestions,
+		setQuestions,
+		navigate,
+	])
+
+	useEffect(() => {
+		if (timer <= 0) {
+			setGame()
+		} else {
+			const intervalId = setInterval(() => {
+				setTimer((t) => t - 1)
+			}, 1000)
+
+			return () => clearInterval(intervalId)
+		}
+	}, [timer, setGame])
+
+	const handleAnswer = (event) => {
+		if (
+			parseInt(event.target.value, 10) ===
+			questions[currentQuestionIndex].answer
+		) {
+			setScore((s) => s + 1)
+			setAnsweredQuestions((aqs) => [
+				...aqs,
+				questions[currentQuestionIndex],
+			])
+			setCurrentQuestionIndex((ci) => ci + 1)
+			event.target.value = ''
+		}
+	}
 
 	return (
 		<div id="game-page">
-			<h1>Game Page</h1>
-			<p>Questions:</p>
-			<ul>
-				{questions.map((question, index) => (
-					<li
-						key={index}
-					>{`${question.num1} ${question.opType} ${question.num2} = ${question.answer}`}</li>
-				))}
-			</ul>
+			<div>
+				<span>Seconds left: {timer}</span>
+				<span>Score: {score}</span>
+			</div>
+			<div>
+				{questions.length > currentQuestionIndex ? (
+					<>
+						<span>{questions[currentQuestionIndex].num1}</span>
+						<span>{questions[currentQuestionIndex].opType}</span>
+						<span>{questions[currentQuestionIndex].num2}</span>
+						<input
+							type="number"
+							onChange={handleAnswer}
+							ref={inputRef}
+						/>
+					</>
+				) : (
+					<div>
+						<h1>Your Score: {score}</h1>
+						<button onClick={() => navigate('/')}>
+							Change Settings
+						</button>
+						<button
+							onClick={() => {
+								setTimer(duration)
+								inputRef.current.focus()
+							}}
+						>
+							Try Again
+						</button>
+					</div>
+				)}
+			</div>
 		</div>
 	)
 }
--- frontend/src/pages/ScorePage.jsx
+++ frontend/src/pages/ScorePage.jsx
@@ -1,7 +1,81 @@
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import GameContext from '../context/GameContext'
+
+const getQuestionList = (additionRange, multiplicationRange, duration) => {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve([
+				{
+					num1: 1,
+					num2: 2,
+					opType: '+',
+					answer: 3,
+				},
+				{
+					num1: 13,
+					num2: 4,
+					opType: '-',
+					answer: 9,
+				},
+				{
+					num1: 5,
+					num2: 6,
+					opType: '*',
+					answer: 30,
+				},
+				{
+					num1: 56,
+					num2: 8,
+					opType: '/',
+					answer: 7,
+				},
+			])
+		}, 2000) // simulate network delay
+	})
+}
+
 const ScorePage = () => {
+	const {
+		score,
+		setScore,
+		duration,
+		setDuration,
+		additionRange,
+		setAdditionRange,
+		multiplicationRange,
+		setMultiplicationRange,
+		setQuestions,
+	} = useContext(GameContext)
+	const navigate = useNavigate()
+
+	const handleTryAgain = () => {
+		setScore(0)
+
+		getQuestionList(additionRange, multiplicationRange, duration).then(
+			(questionList) => {
+				setQuestions(questionList)
+				navigate('/game')
+			}
+		)
+
+		navigate('/game')
+	}
+
+	const handleChangeSettings = () => {
+		setScore(0)
+		setDuration(120)
+		setAdditionRange({ min: 2, max: 100 })
+		setMultiplicationRange({ min: 2, max: 12 })
+
+		navigate('/')
+	}
+
 	return (
 		<div id="score-page">
-			<h1>Score Page</h1>
+			<h1>Your Score: {score}</h1>
+			<button onClick={handleChangeSettings}>Change Settings</button>
+			<button onClick={handleTryAgain}>Try Again</button>
 		</div>
 	)
 }
