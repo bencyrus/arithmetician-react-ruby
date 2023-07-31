b8b21f13e62da716eecc620573650f4e33a4e75a
Ben Cyrus <62337982+bencyrus@users.noreply.github.com>
2023-07-31 04:39:50 +0000

    past game details query complete

--- frontend/src/components/PastGamesListItem.jsx
+++ frontend/src/components/PastGamesListItem.jsx
@@ -1,8 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { ClipLoader } from 'react-spinners'
 import { AiOutlineDown, AiOutlineUp } from 'react-icons/ai'
 
 const PastGamesListItem = ({ game }) => {
 	const [isOpen, setIsOpen] = useState(false)
+	const [gameDetails, setGameDetails] = useState(null)
+	const [isLoading, setIsLoading] = useState(false)
+
+	useEffect(() => {
+		if (isOpen) {
+			setIsLoading(true)
+
+			fetch(`/api/v1/games/${game.id}`)
+				.then((res) => res.json())
+				.then((data) => {
+					setGameDetails(data)
+					setIsLoading(false)
+				})
+				.catch((err) => console.log(err))
+		}
+	}, [isOpen, game.id])
 
 	const handleToggle = () => {
 		setIsOpen(!isOpen)
@@ -20,8 +37,47 @@ const PastGamesListItem = ({ game }) => {
 			</div>
 			{isOpen && (
 				<div style={{ marginLeft: '1rem' }}>
-					<p>Game ID: {game.id}</p>
-					<p>Game Settings ID: {game.gameSettingsId}</p>
+					{isLoading ? (
+						<div
+							style={{
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center',
+								height: '50px',
+							}}
+						>
+							<ClipLoader color="#000" />
+						</div>
+					) : gameDetails ? (
+						<>
+							<p>Duration: {gameDetails.settings.duration}</p>
+							<p>
+								Addition Range: [
+								{gameDetails.settings.addition_range_start}..
+								{gameDetails.settings.addition_range_end}]
+							</p>
+							<p>
+								Multiplication Range: [
+								{
+									gameDetails.settings
+										.multiplication_range_start
+								}
+								..
+								{gameDetails.settings.multiplication_range_end}]
+							</p>
+							<p>Answered Questions:</p>
+							<ul>
+								{gameDetails.answeredQuestions.map(
+									(question) => (
+										<li key={question.id}>
+											{question.num1} {question.op_type}{' '}
+											{question.num2} = {question.answer}
+										</li>
+									)
+								)}
+							</ul>
+						</>
+					) : null}
 				</div>
 			)}
 			<hr />
